a50f6e5b1a063e7313dde1d315dcb0537671c440
Nilay <54245048+dnilay@users.noreply.github.com>
2021-11-24 12:39:57 +0530

    Add files via upload

--- spring-app/src/main/java/org/example/App.java
+++ spring-app/src/main/java/org/example/App.java
@@ -4,6 +4,12 @@ import org.example.model.Coach;
 import org.example.model.Person;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
 public class App
 {
     public static void main( String[] args )
@@ -15,9 +21,20 @@ public class App
             /*Person thePerson=context.getBean("person",Person.class);
             System.out.println(thePerson);*/
 
-            Coach myCoach=context.getBean("theCoach",Coach.class);
+           /* Coach myCoach=context.getBean("theCoach",Coach.class);
             System.out.println(myCoach.getDailyWorkout());
-            System.out.println(myCoach.getDailyFortune());
+            System.out.println(myCoach.getDailyFortune());*/
+
+            DataSource dataSource=context.getBean("dataSource",DataSource.class);
+            Connection connection= dataSource.getConnection();
+            Statement statement=connection.createStatement();
+            ResultSet resultSet=statement.executeQuery("select * from customer");
+            while (resultSet.next())
+            {
+                System.out.println(resultSet.getInt(1)+" "+resultSet.getString(2)+" "
+                + resultSet.getString(3));
+            }
+
 
         }
         catch (Exception e)

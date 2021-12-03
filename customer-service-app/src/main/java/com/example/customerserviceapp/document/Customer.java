1f01503a1291df39eda7c46b549e7456e120663b
Nilay <54245048+dnilay@users.noreply.github.com>
2021-12-03 17:00:25 +0530

    Add files via upload

--- customer-service-app/src/main/java/com/example/customerserviceapp/document/Customer.java
+++ customer-service-app/src/main/java/com/example/customerserviceapp/document/Customer.java
@@ -3,7 +3,6 @@ package com.example.customerserviceapp.document;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.apache.tomcat.jni.Address;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
@@ -15,7 +14,7 @@ public class Customer {
     private Integer customerId;
     private String customerName;
     private String customerEmail;
-
+    private Address address;
 
 
 }
